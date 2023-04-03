935619cad59815108c9313b9189bf89478d03b08
Den1ska14 <qp001@mail.ru>
2023-04-03 17:12:45 +0700

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val numberOrders: Int = 75
+    val thanksOfOrder: String = "Спасибо что воспользовались нашим интернет-магазином! :)"
+    println("$thanksOfOrder Кол-во заказов: $numberOrders")
+}
